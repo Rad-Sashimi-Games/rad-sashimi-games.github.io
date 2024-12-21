8402d0f3c11fdaf410b6f31e6d25edb5eef7d240
Osaze Shears <yoze15@gmail.com>
2024-12-21 08:06:06 -0500

    Attempt to fix broken image links and modified rss feed

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,8 +2,8 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	// Load Markdown and MDX files in the `src/content/devlogs/` directory.
+	loader: glob({ base: './src/content/devlogs', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
