763efb92e4173cd2728f52056a35f1f7d6672ee7
Osaze Shears <yoze15@gmail.com>
2025-01-08 16:52:38 -0500

    Added new tab on the website for the game design document

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import expressiveCode from "astro-expressive-code"
+import remarkMermaid from 'remark-mermaidjs'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [expressiveCode(), mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkMermaid]
+	},
+	redirects:{
+		'/lonnies-fishtank/gdd': '/lonnies-fishtank'
+	}
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -15,4 +15,18 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+const gdd = defineCollection({
+	// Load Markdown and MDX files in the `src/content/devlogs/` directory.
+	loader: glob({ base: './src/content/pages/lonnies-fishtank/gdd', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+export const collections = { blog, gdd };
