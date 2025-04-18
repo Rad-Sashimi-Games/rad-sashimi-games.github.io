4fd620a8175f645e780815685e51a9813ba00e26
Osaze Shears <yoze15@gmail.com>
2025-04-18 13:51:32 -0400

    Updated titles to rad sashimi games

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Fish Head Games';
-export const SITE_DESCRIPTION = 'Welcome to Fish Head Games!';
+export const SITE_TITLE = 'Rad Sashimi Games';
+export const SITE_DESCRIPTION = 'Welcome to Rad Sashimi Games!';
